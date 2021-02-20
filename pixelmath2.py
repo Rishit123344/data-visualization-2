406ed150da497f3f401a6f0353c8bf5f0cb0a6c9
Sai Rishit.S <56475400+Rishit123344@users.noreply.github.com>
2021-02-20 15:33:31 +0530

    Add files via upload

---
+++ pixelmath2.py
@@ -0,0 +1,8 @@
+import pandas as pd 
+import csv
+import plotly.graph_objects as go 
+df = pd.read_csv('pixelmath.csv')
+studentdf = df.loc[df['student_id']=='TRL_zet']
+print(studentdf.groupby('level')['attempt'].mean())
+fig = go.Figure(go.Bar(x = studentdf.groupby('level')['attempt'].mean(), y = ['level1','level2','level3','level4'],orientation = 'h'  ))
+fig.show()
